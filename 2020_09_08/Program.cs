d92c503492f5082963db41e5c0312e109684f99a
ViczianGabor <viczian.gabor13@gmail.com>
2020-09-08 11:22:10 +0200

    A felhasználó választ

--- 2020_09_08/Program.cs
+++ 2020_09_08/Program.cs
@@ -14,7 +14,11 @@ namespace _2020_09_08
             string[] lehetoseg = new string[] { "Kő", "Papír", "Olló" };
             int gepValaszt = vel.Next(0, 3);
             Console.WriteLine("Gép választása: {0}",lehetoseg[gepValaszt]);
-            
+            int jatekosValasz;
+            Console.WriteLine("Kő (0), Papír(1), Olló(2)");
+            Console.WriteLine("Válassz: ");
+            jatekosValasz = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Játékoks választása : {0}",lehetoseg[jatekosValasz]);
             Console.ReadKey();
         }
     }
